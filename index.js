6d8d0deb1020bca3698172440b3e99555aaf8917
Miyuki Abe <miyuki0809@gmail.com>
2018-04-19 02:36:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function destructivelyPrependKitten(name) {
   return kittens
 }
 
-function destructivelyRemoveLastKitten(){
-  kittens.pop()
+function destructivelyRemoveLastKitten(name){
+  kittens.pop(name)
   return kittens
 }
