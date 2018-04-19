176836323e049f31c4514876e6958a9e1c212873
Miyuki Abe <miyuki0809@gmail.com>
2018-04-19 02:33:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const app = "I don't do much."
 
 
 
-function destructivelyAppendKitten() {
-  kittens.push("Ralph")
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
   return kittens
 }
 
-function des
+function destructivelyPrependKitten
