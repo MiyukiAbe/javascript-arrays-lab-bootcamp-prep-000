d578fc5788abea6eec1bc461811efe4a76f9e73c
Miyuki Abe <miyuki0809@gmail.com>
2018-04-19 02:27:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const app = "I don't do much."
 function destructivelyAppendKitten(Array, element) {
   return appendArray(element)
 }
+
+ function destructivelyAppendKitten(array, name) {
+   function appendarray(name) {
+     return destructivelyAppendKitten(name)
+   }
+ }
+   
+  
