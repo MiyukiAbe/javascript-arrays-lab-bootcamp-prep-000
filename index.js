df0d0a47c3bea1d88b29ff5d7ed557960d190a9d
Miyuki Abe <miyuki0809@gmail.com>
2018-04-19 02:29:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,12 +4,7 @@ function destructivelyAppendKitten(Array, element) {
   return appendArray(element)
 }
 
- function destructivelyAppendKitten(arry, name) {
-   function appendarray(name) {
-     return destructivelyAppendKitten(name)
-   }
- }
-   
+
 
 function destructivelyAppendKitten() {
   appendKitten("Ralph")
